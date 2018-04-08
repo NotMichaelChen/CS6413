ed458863f4f50f2706f5e3294dbef376f835a417
NotMichaelChen <mzc12345@gmail.com>
2018-04-08 17:55:57 -0400

    Add option to redirect output to files

--- main.cpp
+++ main.cpp
@@ -3,16 +3,38 @@
 //Contains main function that passes the given filename to the parser
 
 #include <iostream>
+#include <fstream>
 
 #include "parser.hpp"
 
 int main(int argc, char** argv)
 {
-    if(argc != 2)
+    if(argc < 2)
     {
-        std::cerr << "Usage:\t" << argv[0] << " filename\n";
+        std::cerr << "Usage:\t" << argv[0] << " filename redirect_to_file\n";
+        std::cerr << "Pass in anything for the third argument to redirect output to files" << std::endl;
         exit(1);
     }
+    if(argc == 3)
+    {
+        //Redirect output to files
+        std::string filename(argv[1]);
 
-    parse(argv[1]);
+        std::ofstream outstream(filename + "-out.txt");
+        std::streambuf* coutbuf = std::cout.rdbuf();
+        std::cout.rdbuf(outstream.rdbuf());
+
+        std::ofstream errstream(filename + "-err.txt");
+        std::streambuf* cerrbuf = std::cerr.rdbuf();
+        std::cerr.rdbuf(errstream.rdbuf());
+
+        parse(argv[1]);
+
+        std::cerr.rdbuf(cerrbuf);
+        std::cout.rdbuf(coutbuf);
+    }
+    else
+    {
+        parse(argv[1]);
+    }
 }
